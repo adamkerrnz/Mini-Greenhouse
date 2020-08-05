63d445cde1a9fd729d32f42df97185592be2732d
adamkerrnz <kerr.adam@gmail.com>
2020-08-05 17:33:44 +1200

    Add files via upload

--- AKGreenhouse/AKGreenhouse.ino
+++ AKGreenhouse/AKGreenhouse.ino
@@ -1,16 +1,17 @@
-#include <Wire.h>
+#include <Adafruit_Sensor.h>
 #include <DHT.h>
 #include <DHT_U.h>
+#include <Wire.h>
 #include <RTClib.h>
 #include <LiquidCrystal.h>
 
-#define DHTPIN 2
-#define DHTTYPE DHT11
+#define DHTPIN 0
+#define DHTTYPE    DHT11
 #define soilPin A1
-#define pumpPin 5
+#define pumpPin 1
 #define soilPower 4
 
-DHT_Unified dht1(DHTPIN, DHTTYPE);
+DHT_Unified dht(DHTPIN, DHTTYPE);
 RTC_DS1307 rtc;
 LiquidCrystal lcd(8, 9, 4, 5, 6, 7);
 
@@ -22,6 +23,12 @@ char daysOfTheWeek[7][12] = {"Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"};
 void setup() {
   Serial.begin(9600);
   lcd.begin(16, 2);
+  dht.begin();
+  sensor_t sensor;
+  dht.temperature().getSensor(&sensor);
+  dht.humidity().getSensor(&sensor);
+  delayMS = sensor.min_delay / 1000;
+  rtc.begin();
 
   if (! rtc.begin()) {
     lcd.println("Couldn't find RTC");
@@ -31,38 +38,24 @@ void setup() {
 
   if (! rtc.isrunning()) {
     lcd.println("RTC is NOT running, let's set the time!");
-    // When time needs to be set on a new device, or after a power loss, the
-    // following line sets the RTC to the date & time this sketch was compiled
-    // rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
-    // This line sets the RTC with an explicit date & time, for example to set
-    // January 21, 2014 at 3am you would call:
-    //rtc.adjust(DateTime(2014, 1, 21, 3, 0, 0));
-  }
+    rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
 
+  }
 
   pinMode(soilPower, OUTPUT);
   pinMode(pumpPin, OUTPUT);
   digitalWrite(soilPower, LOW);
-  dht1.begin();
-  rtc.begin();
-  sensor_t sensor;
-  dht1.temperature().getSensor(&sensor);
-  dht1.humidity().getSensor(&sensor);
-  delayMS = sensor.min_delay / 1000;
+  digitalWrite(pumpPin, LOW);
+
 }
 
 void loop() {
-  lcd.flush();
   delay(delayMS);
-  // Sets the Relay on or off
-  if (readSoil() < 100)
-    digitalWrite(pumpPin, HIGH);
-  else
-    digitalWrite(pumpPin, LOW);
-
-  displayLCD(); //calls the displayLCD fuction for showing information
+  displayLCDTime();
   printSoilStatus();
+  getTempHum();
 
+ 
 }
 
 int readSoil()
@@ -74,12 +67,10 @@ int readSoil()
   return moistVAL;
 }
 
-void displayLCD()
+void displayLCDTime()
 {
   lcd.setCursor(0, 0);
   DateTime now = rtc.now();
-  lcd.print(daysOfTheWeek[now.dayOfTheWeek()]);
-  lcd.print(" ");
   printDigits(now.hour());
   lcd.print(":");
   printDigits(now.minute());
@@ -95,26 +86,42 @@ void printDigits(byte digits)
 
 void printSoilStatus()
 {
-  const int AirValue = 520;
-  const int WaterValue = 260;
+  const int AirValue = 533;
+  const int WaterValue = 270;
   int intervals = (AirValue - WaterValue) / 3;
   int soilMoistureValue = 0;
   soilMoistureValue = analogRead(soilPin);
-  
+  lcd.setCursor(0, 1);
   if (soilMoistureValue > WaterValue && soilMoistureValue < (WaterValue + intervals))
   {
-    lcd.setCursor(0, 1);
     lcd.print("Very Wet");
+    digitalWrite(pumpPin, LOW);
   }
   else if (soilMoistureValue > (WaterValue + intervals) && soilMoistureValue < (AirValue - intervals))
   {
-    lcd.setCursor(0, 1);
-    lcd.print("Wet");
+    lcd.print("Wet     ");
+    digitalWrite(pumpPin, LOW);
   }
   else if (soilMoistureValue < AirValue && soilMoistureValue > (AirValue - intervals))
   {
-    lcd.setCursor(0, 1);
-    lcd.print("Dry");
+    lcd.print("Dry     ");
+    digitalWrite(pumpPin, HIGH);
   }
   delay(100);
+}
+
+void getTempHum()
+{
+  sensors_event_t event;
+  dht.temperature().getEvent(&event);
+  lcd.setCursor(9, 0);
+  lcd.print("T");
+  lcd.print(event.temperature);
+  lcd.print("C");
+  dht.humidity().getEvent(&event);
+  lcd.setCursor(9,1);
+  lcd.print("H");
+  lcd.print(event.relative_humidity);
+  lcd.print("%");
+  
 }
